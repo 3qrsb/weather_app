76a5431952e68e819a113f586a9ff510107a2032
Yers <eabuov4@gmail.com>
2024-07-21 01:28:24 +0500

    tests, containerization

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
+# Weather Forecast
 
+This web application allows users to enter the name of a city and receive a weather forecast. The application features auto-completion for city names, tracks the history of city searches for each user, and provides an API to view the number of times each city was searched.
+
+## Technologies Used
+
+- **Backend Framework**: Django
+- **API for Weather Data**: OpenWeatherAPI
+- **Containerization**: Docker
+- **Other Libraries**:
+  - `requests` for making API calls
+  - `python-dotenv` for environment variable management
+
+## Installation
+
+1. **Clone the Repository**
+
+```
+git clone <repository-url>
+cd weather_app
+```
+
+2. **Build the Docker Containers**
+
+```
+docker-compose build
+```
+
+3. **Run the Docker Containers**
+
+```
+docker-compose up
+```
+
+4. **Access the Application**
+   Open your web browser and navigate to http://localhost:8000/.
+
+## Running Tests
+
+```
+docker-compose exec web python manage.py test
+```
--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class WeatherAppTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username='test_user')
+
+    def test_get_weather_view(self):
+        response = self.client.get(reverse('get_weather'), {'city': 'Astana'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'weather/index.html')
+
+    def test_autocomplete_view(self):
+        response = self.client.get(reverse('autocomplete'), {'term': 'Ast'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+        
+    def test_search_count_view(self):
+        response = self.client.get(reverse('search_count'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+        
+    def test_search_count_increment(self):
+        initial_response = self.client.get(reverse('get_weather'), {'city': 'Astana'})
+        self.assertEqual(initial_response.status_code, 200)
+
+        initial_count_response = self.client.get(reverse('search_count'))
+        initial_counts = initial_count_response.json()
+        initial_count = next((item['count'] for item in initial_counts if item['city'] == 'Astana'), 0)
+
+        self.client.get(reverse('get_weather'), {'city': 'Astana'})
+
+        incremented_count_response = self.client.get(reverse('search_count'))
+        incremented_counts = incremented_count_response.json()
+        incremented_count = next((item['count'] for item in incremented_counts if item['city'] == 'Astana'), 0)
+
+        self.assertEqual(incremented_count, initial_count + 1)
